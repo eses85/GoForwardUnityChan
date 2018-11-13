4fc82c3044a2443a6c448586059350e55b2ffcdb
0 es <over100degreesfahrenheit@gmail.com>
2018-11-13 17:42:50 +0900

    提出用

---
+++ Assets/BlockSe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSe : MonoBehaviour {
+
+    public AudioSource cubeSE;
+    // Use this for initialization
+    void Start () {
+        cubeSE = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     //   if (Input.GetKeyDown(KeyCode.K))
+   //    {
+   //         cubeSE.PlayOneShot(cubeSE.clip);
+
+        }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+         if(collision.gameObject.tag == "Cube")
+           cubeSE.PlayOneShot(cubeSE.clip);
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+  //  public AudioSource cubeSE;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+      //  cubeSE = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+     //   void OnCollisionEnter(Collision other)
+     //   {
+     //   cubeSE.PlayOneShot(cubeSE.clip);
+         //    }
+        }
